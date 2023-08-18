802914bba3c0728f218132987175da4552b2593f
Darijan Jelušić <darijan.jelusic@gmail.com>
2023-08-18 21:41:21 +0200

    MIDI Playback

--- PlaybackThread.py
+++ PlaybackThread.py
@@ -1,10 +1,41 @@
-import time
+from datetime import datetime, timedelta
 
 from PySide6.QtCore import Slot, QObject, QUrl, QThread, Signal
 from PySide6.QtQml import QmlElement
 from mido import MidiFile, Message
 
 from pyo import *
+
+
+class CustomClock:
+    def __init__(self):
+        self.resume_time = datetime.fromtimestamp(0)
+        self.pause_time = datetime.fromtimestamp(0)
+        self.start_time = datetime.fromtimestamp(0)
+        self.time_diff = 0
+        self.playing = False
+
+    def start(self):
+        self.resume_time = datetime.now()
+        self.pause_time = datetime.now()
+        self.start_time = datetime.now()
+        self.time_diff = 0
+        self.playing = True
+
+    def pause(self):
+        self.pause_time = datetime.now()
+        self.playing = False
+
+    def resume(self):
+        self.resume_time = datetime.now()
+        self.time_diff += (self.resume_time - self.pause_time).total_seconds()
+        self.playing = True
+
+    def get_time(self):
+        if self.playing:
+            return (datetime.now() - self.start_time - timedelta(seconds=self.time_diff)).total_seconds()
+        else:
+            return 0
 
 
 class PlaybackThread(QThread):
@@ -18,13 +49,11 @@ class PlaybackThread(QThread):
         self.playback_stopped = Signal()
 
         self.messages = None
-        self.current_message_index = 0
-        self.current_time = 0.0  # Current time position in MIDI file
-
-        self.metro = Metro(1000)  # Metronome to synchronize MIDI messages
+        self.clock = CustomClock()
 
     def init_file(self):
-        self.messages = self.file.play()
+        self.clock.__init__()
+        self.messages = self.file.play(now=self.clock.get_time)
 
     def run(self):
         while not self.file:
@@ -32,22 +61,31 @@ class PlaybackThread(QThread):
         self.playback()
 
     def playback(self):
-        messages = self.file.play()
         for message in self.messages:
-            # if self.reset:
-            #     return
-            print(message)
             while not self.play:
                 self.msleep(500)
             self.s.addMidiEvent(*message.bytes())
         self.stop_playback()
 
     def start_playback(self):
+        if self.reset:
+            self.play = True
+            self.reset = False
+            self.clock.start()
+        else:
+            self.resume_playback()
+
+    def resume_playback(self):
         self.play = True
         self.reset = False
+        self.clock.resume()
 
     def pause(self):
         self.play = False
+        self.clock.pause()
+        for n in range(128):
+            stop_msg = Message('note_off', note=n)
+            self.s.addMidiEvent(*stop_msg.bytes())
 
     def stop_playback(self):
         self.pause()
